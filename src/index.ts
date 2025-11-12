97f5fa81092c8f8fe0a4a806fc8397bffb04564e
gwyneth-pena <gwenpena21@gmail.com>
2025-11-12 18:14:03 +0800

    Fixed for vercel deployment

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import "reflect-metadata";
 import "dotenv/config";
 import { createServer } from "./server.js";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import type { Application } from "express";
+
+const isLocal = process.env.NODE_ENV=== "development";
+
+let app: Application;
 
 const main = async () => {
-  const app = await createServer();
-  const PORT = Number(process.env.PORT) || 4000;
-  const HOST = process.env.HOST || "localhost";
+  app = await createServer();
 
-  app.listen(PORT, HOST, () => {
-    console.log(`Server running at http://${HOST}:${PORT}`);
-  });
+  if (isLocal) {
+    const PORT = Number(process.env.PORT) || 4000;
+    const HOST = process.env.HOST || "localhost";
+    app.listen(PORT, HOST, () => {
+      console.log(`Server running at http://${HOST}:${PORT}`);
+    });
+  }
 };
 
 main().catch((err) => console.error(err));
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  if (!app) {
+    app = await createServer();
+  }
+
+  app(req, res);
+}
