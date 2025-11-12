1a652d454a73e15aa2c8b8064f8090c6cd6641bf
gwyneth-pena <gwenpena21@gmail.com>
2025-11-12 18:24:43 +0800

    Fixed for vercel deployment

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,6 +1,9 @@
 import { Options } from '@mikro-orm/core';
 import { MySqlDriver } from '@mikro-orm/mysql';
 import 'dotenv/config';
+import { Post } from './entities/Post.js';
+import { User } from './entities/User.js';
+import { Vote } from './entities/Vote.js';
 
 const config: Options<MySqlDriver> = {
   driver: MySqlDriver,
@@ -9,8 +12,7 @@ const config: Options<MySqlDriver> = {
   password: process.env.DB_PASS || '',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
-  entities: ['/dist/entities/*.js'],
-  entitiesTs: ['/src/entities/*.ts'], 
+  entities: [Post, User, Comment, Vote],
   debug: process.env.NODE_ENV === 'development',
   migrations: {
     path: './src/migrations',        // folder for migrations
