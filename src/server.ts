c7b1b21156999af8abe37f4ceb0ae73cb51f0d7b
gwyneth-pena <gwenpena21@gmail.com>
2025-11-27 19:51:45 +0800

    Force redis prefix

--- src/server.ts
+++ src/server.ts
@@ -65,7 +65,7 @@ export async function createServer() {
 
   app.post("/logout", async (req: any, res) => {
     const secret = envConfig.SECRET_KEY;
-    const signedCookie = req.sessionID;
+    const signedCookie = req.cookies.session_id;
     console.log("Logging out", signedCookie, req.cookies.session_id);
     const rawSessionId = cookieParser.signedCookie(signedCookie, secret);
     console.log("Logging out raw", rawSessionId);
